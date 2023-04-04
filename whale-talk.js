5f698db24864652ca4dbcf4d1288515eee662195
hangnail-en <14255092+hangnail-en@users.noreply.github.com>
2023-04-04 17:05:05 -0400

    Update whale-talk.js - Incomplete
    - Output is still not accurate
    - No inclusion of I's or A's for some reason
    - "undefined" error

--- whale-talk.js
+++ whale-talk.js
@@ -1,12 +1,19 @@
-let input = 'Dark Souls';
+let input = 'turpentine and turtles';
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e' || input[i] === 'u') {
+    resultArray.push(input[i]);
+  };
   for (let j = 0; j < vowels.length; j++) {
-    resultArray.push(vowels[j]);
   };
 };
 
-console.log(resultArray.join(' '));
+function resultString() {
+  resultArray = resultArray.join('');
+  console.log(resultArray.toUpperCase());
+};
+
+console.log(resultString());
